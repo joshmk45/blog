164b6af036d085d95fe6b018fe1a6fe8899f02c8
joshmk45 <mwanukejose0627@gmail.com>
2023-04-07 13:01:34 +0300

    components in vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-  <HomePage username="youngjo3" msg="you are welcome" />
-  <contactPage this is contact/>
+<div id="app">
+<!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+<HomePage/>
+</div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-import HomePage from './components/HomePage.vue'
-import contactPage from './components/contactPage.vue'
+import HomePage from './components/homePage.vue'
 export default {
   name: 'App',
   components: {
-    // HelloWorld,
-    HomePage,
-    contactPage
+    HomePage
   }
 }
 </script>
 
 <style>
 #app {
+  margin-top:0;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,68 +1,27 @@
 <template>
-<div>
-<!-- <h1>hello from home component</h1>
-<h1>{{username}}</h1>
-<h1>{{msg}}</h1>
-<button v-on:mouseover="hello('username')">call hello function</button> -->
-<!-- <h1 v-if="show">conditional rendering in vue.js</h1>
-<h1 v-else>else side of above</h1> -->
-<!-- <button v-on:click="showFunction">Toggle</button> -->
-<h1>for loop in vue.js</h1>
-<table>
-    <tr>
-        <th>ID</th><th>NAME</th><th>EMAIL</th>
-    </tr>
-    <tr v-for="user in users" :key="user.id">
-    <td>{{user.id}}</td>
-    <td>{{user.name}}</td>
-    <td>{{user.email}}</td>
-    </tr> 
-</table>
-</div>
+    <div class="home">
+        <h1>this is Home component</h1>
+        <userPage/>
+        <productPage/>
 
-    
+    </div>
 </template>
 <script>
-export default{
-    name:'HomePage',
-    props:{
-        username:String,
-        msg:String
-    },methods:{
-        //method with parameter
-        hello(data)
-        {
-            alert("clicked "+data);
-        },
-        //method without parameter
-        showFunction(){
-            this.show=!this.show
-        }
-
-    },data()
-    {
-        //data used for condition used in template
-        // return{
-        //     show:false
-        // }
-        return{
-            users:[
-                {id:1,name:"jose",email:"mkjosh501@gmail.com"},
-                {id:2,name:"toly",email:"mkjosh501@gmail.com"},
-                {id:3,name:"adi",email:"mkjosh501@gmail.com"},
-                {id:4,name:"part",email:"mkjosh501@gmail.com"},
-            ]
-        }
-    },
+import userPage from './userPage.vue'
+import productPage from './productsPage.vue'
+export default {
+    name:"HomePage",
+    components:{
+        userPage,
+        productPage
+    }
 }
 </script>
 <style>
- /* h1{
-    color:red;
-    text-align: left;
-    margin: 0;
- } */
- table{
-    margin:auto;
- }
+.home{
+    margin-top:0;
+    height:500px;
+    background-color:aquamarine;
+    text-align: center;
+}
 </style>
